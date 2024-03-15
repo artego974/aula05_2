2d77d829474785a0610bf31767114eaa5acd88a5
artego <162368460+artego974@users.noreply.github.com>
2024-03-15 17:25:42 -0300

    Add files via upload

---
+++ pokemon.js
@@ -0,0 +1,15 @@
+let meu_pokemon 
+switch (meu_pokemon) {
+    case 'bulbasauro':
+        console.log("planta e veneno")
+        break;
+        case 'charmander':
+        console.log("fogo")
+        break;
+        case 'squirtle':
+        console.log("agua")
+        break;
+        
+    default:console.log ('outro elemento ou não encontrado')
+        break;
+}
